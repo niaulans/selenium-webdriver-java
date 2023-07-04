c8fb38845eab12d675548f72116576d21727aaeb
niaulans <niaulansari0000@gmail.com>
2023-07-04 13:49:19 +0700

    Setup webdriver

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,43 @@
+package base;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp() {
+        // Set directory chromedriver
+        System.setProperty("webdriver.com.driver", "resources/chromedriver.exe");
+
+        // Create ChromeDriver object
+        driver = new ChromeDriver(); // can be changed to Firefox etc.
+
+        // Load to app
+        driver.get("https://the-internet.herokuapp.com/");
+
+        // Maximize window app
+        driver.manage().window().maximize();
+
+        // Set windows
+//        driver.manage().window().setSize(new Dimension(390, 844));
+
+        // Fullscreen window app
+//        driver.manage().window().fullscreen();
+
+        // Get title of app
+        System.out.println(driver.getTitle());
+
+        // Close app and session
+        driver.quit();
+
+        // Just close the windows, not session
+//        driver.close();
+    }
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
