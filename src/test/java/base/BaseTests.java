dd3c7ee08a622255db9695f1d8295a7829baf3e5
niaulans <niaulansari0000@gmail.com>
2023-07-26 13:56:55 +0700

    take screenshot on failure test case

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,14 +1,20 @@
 package base;
 
-import org.openqa.selenium.WebDriver;;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
 
@@ -40,4 +46,18 @@ public class BaseTests {
     public WindowManager getWindowManager() {
         return new WindowManager(driver);
     }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
