befb2557d4a084d461335935755b4376cfa53173
niaulans <niaulansari0000@gmail.com>
2023-07-26 12:47:22 +0700

    Create navigation test case

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -76,4 +76,9 @@ public class HomePage {
         clickLink("Infinite Scroll");
         return new InfiniteScrollPage(driver);
     }
+
+    public MultipleWindowsPage clickMultipleWindows()  {
+        clickLink("Multiple Windows");
+        return new MultipleWindowsPage(driver);
+    }
 }
